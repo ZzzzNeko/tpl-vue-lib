33b7df6f76e220c1df3992a9624af0f84872ab30
guojian <guojian.pan@ly.com>
2020-07-15 20:36:39 +0800

    build 时清空 types/ 下对应文件

--- script/build.js
+++ script/build.js
@@ -2,25 +2,36 @@ const path = require("path");
 const commander = require("commander");
 const listr = require("listr");
 const execa = require("execa");
+const fse = require("fs-extra");
 const libCfgPath = path.resolve(__dirname, "../config/build.lib.js");
 const appCfgPath = path.resolve(__dirname, "../config/build.app.js");
 
 commander
   .command("build")
   .arguments("[target]")
-  .action(function(target, command) {
+  .action(async function(target, command) {
     const libScript = `webpack --config ${libCfgPath}`;
     const appScript = `webpack --config ${appCfgPath}`;
     if (target === "lib") {
+      await fse.remove(path.resolve(__dirname, "../types/lib"));
       execa(libScript, { shell: true, stdio: "inherit" });
     } else if (target === "app") {
+      await fse.remove(path.resolve(__dirname, "../types/app"));
       execa(appScript, { shell: true, stdio: "inherit" });
     } else {
       // build lib & app
       const tasks = new listr([
         {
+          title: "clear types/lib dir",
+          task: () => fse.remove(path.resolve(__dirname, "../types/lib")),
+        },
+        {
           title: "build lib",
           task: () => execa(libScript, { shell: true, stdio: "inherit" }),
+        },
+        {
+          title: "clear types/app dir",
+          task: () => fse.remove(path.resolve(__dirname, "../types/app")),
         },
         {
           title: "build app",
